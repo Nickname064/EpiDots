2498c81997aafd7dbdd38b24ab9b869eb0226908
Nickname064 <wanthony972@gmail.com>
2024-09-16 23:48:23 +0200

    Improved install script

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
-#Install basic stuff
-nix profile install nixpkgs#kitty
-nix profile install nixpkgs#libnotify && notify-send "Libnotify Installed !"
+#!/bin/bash
 
+#============Install packages=================================
+#=============================================================
 
-#install non-primordial stuff
-nix profile install nixpkgs#dunst && notify-send "Dunst installed !"
-nix profile install nixpkgs#bat && notify-send "Bat installed !" || notify-send "Bat installed failed"
-nix profile install nixpkgs#polybar
-nix profile install nixpkgs#rofi
-nix profile install nixpkgs#neovim
-nix profile install nixpkgs#gh
-nix profile install nixpkgs#lapce
+pkgs=(
+  libnotify dunst kitty
+  polybar bat rofi neovim neovide gh
+  nerdfonts
+)
 
-#Additional, probably long installs
-nix profile install nixpkgs#nerdfonts
-
+for pkg in "${pkgs[@]}"; do
+  if nix profile install "nixpkgs#${pkg}";
+  then
+    notify-send "${pkg} installed successfully !"
+  else
+    notify-send "${pkg} failed to install"
+  fi
+done
 
 notify-send "Installation complete !"
+
+#=============Move files=======================================
+#==============================================================
+
+
+#=============Run jobs=========================================
+#==============================================================
+
+nohup polybar &
+
